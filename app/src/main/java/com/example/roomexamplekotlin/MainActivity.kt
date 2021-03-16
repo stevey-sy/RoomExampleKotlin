d52826c0a2a9d7232f334296e21cdac14b108046
stevey-sy <sinsy89@hanmail.net>
2021-03-16 15:27:21 +0900

    LiveData 적용해서 db 데이터 조회, 자동갱신 기능 추가

--- app/src/main/java/com/example/roomexamplekotlin/MainActivity.kt
+++ app/src/main/java/com/example/roomexamplekotlin/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.room.Room
 
 class MainActivity : AppCompatActivity() {
@@ -20,14 +21,20 @@ class MainActivity : AppCompatActivity() {
                 .build()
 
         val result_text : TextView = findViewById(R.id.result_text)
-        result_text.text = db.todoDao().getAll().toString()
+//        result_text.text = db.todoDao().getAll().toString()
+
+        db.todoDao().getAll()
+                .observe(this, Observer {
+                    result_text.text = it.toString()
+                })
+                .toString()
 
         val edit_text : EditText = findViewById(R.id.todo_edit)
 
         val add_button : Button = findViewById(R.id.add_button)
         add_button.setOnClickListener {
             db.todoDao().insert(Todo(edit_text.text.toString()))
-            result_text.text = db.todoDao().getAll().toString()
+//            result_text.text = db.todoDao().getAll().toString()
         }
     }
 }
